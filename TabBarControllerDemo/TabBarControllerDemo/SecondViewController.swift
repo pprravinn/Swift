74e2ec0689d02a7ec0c7c17218f36802f93a8286
Pravin Kamble <pprravinn@gmail.com>
2017-02-14 17:51:31 +0530

    This is demo of TabBarController.

---
+++ TabBarControllerDemo/TabBarControllerDemo/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  TabBarControllerDemo
+//
+//  Created by Uday Jain on 2/14/17.
+//  Copyright © 2017 Uday Jain. All rights reserved.
+//
+
+import UIKit
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ TabBarControllerDemo/TabBarControllerDemo/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  TabBarControllerDemo
+//
+//  Created by Uday Jain on 2/14/17.
+//  Copyright © 2017 Uday Jain. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
